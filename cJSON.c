5483c4f306e408a719004decf09dc1e7e56ba85c
Miss-you <snowfly1993@gmail.com>
2016-04-27 22:50:55 +0800

    readme

--- cJSON.c
+++ cJSON.c
@@ -403,9 +403,13 @@ static const char *parse_value(cJSON *item,const char *value)
     if (!strncmp(value,"null",4))	{ item->type=cJSON_NULL;  return value+4; }
     if (!strncmp(value,"false",5))	{ item->type=cJSON_False; return value+5; }
     if (!strncmp(value,"true",4))	{ item->type=cJSON_True; item->valueint=1;	return value+4; }
+    /* string数据 */
     if (*value=='\"')				{ return parse_string(item,value); }
+    /* 数值数据 */
     if (*value=='-' || (*value>='0' && *value<='9'))	{ return parse_number(item,value); }
+    /* JSON对象数组 */
     if (*value=='[')				{ return parse_array(item,value); }
+    /* JSON对象 */
     if (*value=='{')				{ return parse_object(item,value); }
     
     ep=value;return 0;	/* failure. */
